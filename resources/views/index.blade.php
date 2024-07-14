65ace8761232439e974ca61e5673653b9d306520
= <=>
2024-07-14 13:57:48 +0900

    Create Card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wedding Card</title>
+    <title>Marenza & Gelian Wedding Invitation</title>
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://kit.fontawesome.com/4419d23bf4.js" crossorigin="anonymous"></script>
